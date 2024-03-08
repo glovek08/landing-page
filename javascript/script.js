7426dc365616ccc88979562d6f91bfbff3edc840
Gabriel Barn <gbrielbbarn@gmail.com>
2024-03-08 15:36:57 -0300

    [feat] solution-grid scroll on hover

--- javascript/script.js
+++ javascript/script.js
@@ -8,3 +8,15 @@ window.addEventListener('scroll', () => {
         header.classList.remove('transparent');
     }
 });
+document.addEventListener("DOMContentLoaded", () => {
+    let solutionGrid = document.getElementById("solution-grid");
+    let solutionGridWidth = solutionGrid.offsetWidth;
+    let solutionGridRightNav = document.getElementById('solution-grid-right-nav');
+    let solutionGridLeftNav = document.getElementById('solution-grid-left-nav');
+    solutionGridLeftNav.addEventListener("mouseover", () => {
+        solutionGrid.scrollLeft += -solutionGridWidth;
+    });
+    solutionGridRightNav.addEventListener("mouseover", () => {
+        solutionGrid.scrollLeft += solutionGridWidth;
+    });
+});
