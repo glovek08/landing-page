1bef1d7604db94284f00c6c40f04e042ba694521
Gabriel Barn <gbrielbbarn@gmail.com>
2024-03-08 20:00:30 -0300

    [feat] solution-grid-item active state

--- javascript/script.js
+++ javascript/script.js
@@ -1,22 +1,29 @@
 
 
-window.addEventListener('scroll', () => {
+window.addEventListener('scroll', () => { //Scrolls the header when user scrolls down.
     const header = document.getElementById('header');
-    if (window.scrollY > 5) { // Adjust the scroll amount as needed
+    if (window.scrollY > 5) {
         header.classList.add('transparent');
     } else {
         header.classList.remove('transparent');
     }
 });
 document.addEventListener("DOMContentLoaded", () => {
-    let solutionGrid = document.getElementById("solution-grid");
-    let solutionGridWidth = solutionGrid.offsetWidth;
-    let solutionGridRightNav = document.getElementById('solution-grid-right-nav');
-    let solutionGridLeftNav = document.getElementById('solution-grid-left-nav');
-    solutionGridLeftNav.addEventListener("mouseover", () => {
-        solutionGrid.scrollLeft += -solutionGridWidth;
+    let solutionGrid = document.getElementById('solution-grid');
+    solutionGrid.addEventListener('mouseover', (e) => {
+        e.target.addEventListener('mouseover', () => {
+        })
     });
-    solutionGridRightNav.addEventListener("mouseover", () => {
-        solutionGrid.scrollLeft += solutionGridWidth;
-    });
+    //Scrolls X the solution-grid when user hover over either solution-nav.
+    // let solutionGrid = document.getElementById("solution-grid");
+    // let solutionGridItems = Array.from(document.getElementsByClassName("solution-grid-item"));
+    // let solutionGridRightNav = document.getElementById('solution-grid-right-nav');
+    // let solutionGridLeftNav = document.getElementById('solution-grid-left-nav');
+    // solutionGridLeftNav.addEventListener("mouseover", () => {
+    //     solutionGrid.scrollLeft += -solutionGridItems[0].scrollWidth; //Gets the width of a solution-grid-item;
+    // });
+    // solutionGridRightNav.addEventListener("mouseover", () => {
+    //     solutionGrid.scrollLeft += solutionGridItems[0].scrollWidth;
+    //     console.log(solutionGridItems[0].scrollWidth);
+    // });
 });
