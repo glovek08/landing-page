121deb99169d877daa091ed2d1271c87e858daa5
Gabriel Barn <gbrielbbarn@gmail.com>
2024-03-10 20:23:33 -0300

    [feat] solution-grid onmouseover scroll

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,4 @@
 
-
 window.addEventListener('scroll', () => { //Scrolls the header when user scrolls down.
     const header = document.getElementById('header');
     if (window.scrollY > 5) {
@@ -9,25 +8,32 @@ window.addEventListener('scroll', () => { //Scrolls the header when user scrolls
     }
 });
 document.addEventListener("DOMContentLoaded", () => {
-    let solutionGridElements = document.getElementsByClassName('solution-grid-item');
-    let solutionGrid = document.getElementById('solution-grid');
-
+    const solutionGridElements = document.getElementsByClassName('solution-grid-item');
+    const solutionGrid = document.getElementById('solution-grid');
+    const solutionGridRightNav = document.getElementById('solution-grid-right-nav');
+    const solutionGridLeftNav = document.getElementById('solution-grid-left-nav');
     Array.from(solutionGridElements).forEach(element => {
-        element.addEventListener('click', (e) => {
+        element.onclick = (event) => {
             let currentActiveElement = document.querySelectorAll('.solution-grid-item.active');
             //This is to prevent the h3 solution-title from getting active.
-            currentActiveElement.forEach(e => {
-                e.classList.remove('active');
+            currentActiveElement.forEach(currentActiveEl => {
+                currentActiveEl.classList.remove('active');
             });
-            e.currentTarget.classList.add('active');
+            event.currentTarget.classList.add('active');
             let containerRect = solutionGrid.getBoundingClientRect();
-            let elementRect = e.currentTarget.getBoundingClientRect();
-            let scrollLeft = elementRect.left - containerRect.left - (containerRect.width - elementRect.width) / 2;
+            let elementRect = event.currentTarget.getBoundingClientRect();
+            let scrollLeftOnClick= elementRect.left - containerRect.left - (containerRect.width - elementRect.width) / 2;
             solutionGrid.scrollTo({
-                left: scrollLeft,
+                left: scrollLeftOnClick,
                 behavior: 'smooth'
             });
-        });
+        };
     });
+    solutionGridRightNav.onmouseover = () => {
+        console.log('scrollRight');
+    }
+    solutionGridLeftNav.onmouseover = () => {
+        console.log('scrollLeft');
+    }
 });
 //TODO: DYNAMICALLY CHANGE THE TEXT CONTENT OF THE SOLUTION-CONTEXT TO THE CURRENTLY ACTIVE SOLUTION-GRID-ITEM.
